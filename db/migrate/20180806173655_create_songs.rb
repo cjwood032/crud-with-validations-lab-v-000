12b33f167d6e7bea96a837a14d4a3a11aaa4e319
Christopher Wood <cjwood032@gmail.com>
2018-08-06 17:53:36 +0000

    automatically backed up by learn

--- db/migrate/20180806173655_create_songs.rb
+++ db/migrate/20180806173655_create_songs.rb
@@ -3,9 +3,10 @@ class CreateSongs < ActiveRecord::Migration
     create_table :songs do |t|
       t.string :title
       t.boolean :released
+      t.integer :release_year
       t.string :artist_name
       t.string :genre
-      
+
 
       t.timestamps null: false
     end
